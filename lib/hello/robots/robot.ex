cebc6a665c8a348f6c029e524f493868bbc692ce
Finn <finnterdal@gmail.com>
2019-10-08 13:58:34 -0500

    image_url is now just a view method, duh

--- lib/hello/robots/robot.ex
+++ lib/hello/robots/robot.ex
@@ -5,20 +5,14 @@ defmodule Hello.Robots.Robot do
   schema "robots" do
     field :name, :string, default: ""
     field :fuel_level, :integer, default: 0
-    field :image_url, :string, virtual: true, default: "https://robohash.org/robot"
-
     timestamps()
   end
 
   @doc false
   def changeset(robot, attrs) do
-    IO.inspect(attrs)
-    IO.inspect(robot)
-
     robot
-    |> cast(attrs, [:name, :fuel_level, :image_url])
-    # |> validate_required([:name, :fuel_level, :image_url])
-    # Check to confirm this is right:
-    # |> validate_inclusion(:fuel_level, 0..100)
+    |> cast(attrs, [:name, :fuel_level])
+    |> validate_required([:name, :fuel_level])
+    |> validate_inclusion(:fuel_level, 0..100)
   end
 end
--- priv/repo/migrations/20191008155022_image_url_virtual.exs
+++ priv/repo/migrations/20191008155022_image_url_virtual.exs
@@ -4,10 +4,7 @@ defmodule Hello.Repo.Migrations.ImageUrlVirtual do
   def change do
     alter table(:robots) do
       remove :image_url
-      # Huh, none of these approaches seem to work.
-      # Even the brand new dummy column does show up in Postgres table 🤔
-      # add :image_url, :string, virtual: true, default: "https://robohash.org/robot"
-      # add :dummy, :integer, virtual: true
+
     end
   end
 end
